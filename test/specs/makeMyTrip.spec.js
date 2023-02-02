91f3f64445245e2eb02c78b5f2dc736397efe862
Anil Ca <anilcaalexander@gmail.com>
2023-02-02 12:33:13 +0530

    selenium standalone-service

---
+++ test/specs/makeMyTrip.spec.js
@@ -0,0 +1,16 @@
+const { homePage } = require("../pageobjects/home_page"); //Importing Homepage object
+const { flightsPage } = require("../pageobjects/flightsPage");//Importing Flights page object
+const { completeBookingPage } = require("../pageobjects/completeBookPage");
+
+describe('Work flow for Make my trip', () => {
+	it('User should be able to load url', async () => {
+		await homePage.openUrl();
+		await expect(browser).toHaveUrl('https://www.makemytrip.com/');
+	});
+	it('the login page should have a header displayed', async () => {
+		expect(await homePage.$homePageHeader().isDisplayed()).toBe(true, 'Expect homepage header to be displayed');
+		await browser.pause(500);
+	});
+
+});
+
