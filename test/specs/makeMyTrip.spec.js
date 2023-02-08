32a3bf9e7cd77c508c674c6b9f5e1d7c07e36172
AnilCa1905 <114649299+AnilCa1905@users.noreply.github.com>
2023-02-08 15:31:00 +0530

    Update makeMyTrip.spec.js

--- test/specs/makeMyTrip.spec.js
+++ test/specs/makeMyTrip.spec.js
@@ -5,7 +5,7 @@ const { completeBookingPage } = require("../pageobjects/completeBookPage");
 describe('Work flow for Make my trip', () => {
 	it('User should be able to load url', async () => {
 		await homePage.openUrl();
-		await expect(browser).toHaveUrl('https://www.makemytrip.com/');
+		await expect(browser).toHaveUrl('https://www.testingmavens.com/');
 	});
 	it('the login page should have a header displayed', async () => {
 		expect(await homePage.$homePageHeader().isDisplayed()).toBe(true, 'Expect homepage header to be displayed');
