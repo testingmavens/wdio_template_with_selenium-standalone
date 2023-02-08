faeebf549aeb3c268b973bb5c853957de5939357
AnilCa1905 <114649299+AnilCa1905@users.noreply.github.com>
2023-02-08 15:32:56 +0530

    Update common.js

--- test/pageobjects/common.js
+++ test/pageobjects/common.js
@@ -2,11 +2,11 @@ module.exports = class Common {
   constructor()
   {
 this.$homePageHeader = () => $(`//div[@class='whiteText makeFlex perfectCenter langSlct']`);
-this.$logo = () => $(`//img[@alt='Make My Trip']`)
+this.$logo = () => $(`//a[text()="Our services"]`)
   }
   async openUrl()
   {
-    await browser.url(`https://www.makemytrip.com/`);
+    await browser.url(`https://www.testingmavens.com/`);
     await browser.maximizeWindow();
    await this.$homePageHeader().waitForDisplayed(10000,true,'Home pahe header is not displayed');
   }
