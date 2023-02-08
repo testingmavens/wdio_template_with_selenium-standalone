0be9b54d0275c01114b8d779a043d02088e755ff
Anil Ca <anilcaalexander@gmail.com>
2023-02-08 15:55:44 +0530

    Template for Selenium-standalone

--- wdio.conf.js
+++ wdio.conf.js
@@ -83,7 +83,7 @@ args:[
     '--incognito',
     '--headless',
   ],
-        }
+}
     }],
   //
   // ===================
@@ -92,7 +92,7 @@ args:[
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: "silent",
+  logLevel: "trace",
   //
   // Set specific log levels per logger
   // loggers:
